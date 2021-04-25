4bcdba287e74054ace8d72dbd3896a2401f1554c
Balazs Kovacs <balazs.kovacs@stylersonline.com>
2021-04-25 21:27:45 +0200

    Implement full REST for Tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,9 +6,20 @@ router.get('/', async (req, res, next) => {
   res.json(await Task.find());
 });
 
+router.get('/:id', async (req, res, next) => {
+  res.json(await Task.findById(req.params.id));
+});
+
 router.post('/', async (req, res, next) => {
-  const task = new Task({ name: 'Test Task' });
-  res.json(await task.save());
+  res.json(await Task.create(req.body));
 })
+
+router.patch('/:id', async (req, res, next) => {
+  res.json(await Task.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+});
+
+router.delete('/:id', async (req, res, next) => {
+  res.json(await Task.findByIdAndDelete(req.params.id));
+});
 
 module.exports = router;
